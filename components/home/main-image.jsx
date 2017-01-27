67e7ccb2be0a5508a98a472b27ac5d6bde4b1592
Greg Pfaff <greg.pfaff@razorfish.com>
2017-01-27 10:44:29 -0600

    removing error

--- components/home/main-image.jsx
+++ components/home/main-image.jsx
@@ -46,7 +46,7 @@ class MainImage extends Component {
         <Slider {...settings}>
           {imgs.map(function(imgs){
             console.log(imgs);
-            return <img onLoad={sliderRefresh()}key={imgs.id} alt={imgs.name} src={imgs.src}/>
+            return <img key={imgs.id} alt={imgs.name} src={imgs.src}/>
           })}
         </Slider>
       </div>
